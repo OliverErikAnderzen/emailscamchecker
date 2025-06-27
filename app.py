998fdabd42cac986baf516f5e7315d03f2c89e5e
OliverErikAnderzen <olivererikanderzen@gmail.com>
2025-06-27 21:40:57 +0200

    Setup

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def send_simple_message():
+  	return requests.post(
+  		f"https://api.eu.mailgun.net/v3/{os.getenv('MAILGUN_DOMAIN')}/messages",
+  		auth=("api", os.getenv('MAILGUN_API_KEY')),
+  		data={"from": f"Mailgun Sandbox <{os.getenv('SENDER_EMAIL')}>",
+			"to": f"Oliver <{os.getenv('PERSONAL_EMAIL')}>",
+  			"subject": "Hello Oliver",
+  			"text": "Congratulations Oliver, you just sent an email with Mailgun! You are truly awesome!"})
+
+send_simple_message()
+print("Email sent successfully!")
